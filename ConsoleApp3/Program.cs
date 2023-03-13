2747d94fd2bc553251490a0fb2c06f27a8dee5d5
ZcepZtar <seb.stjernholm@gmail.com>
2023-03-13 08:42:44 +0100

    Logos in the house

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleApp3
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Word");
+            Console.WriteLine("Hey Logos");
         }
     }
 }
